fdb2dc706dd4e932b0351e9f4f731bcb318ec14c
Исп-34 <-34@LAB21-12>
2024-10-30 11:14:12 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Lib_1
+{
+    public class Class1
+    {
+        private Random random = new Random();
+
+        // Метод для вычисления разницы целых случайных чисел
+        public  static void CalculateDifference(int k, out int sum, out string susm, out int count)
+        {
+            sum = 0;
+            count = 0;
+            susm = "";
+            int lastNumber = 0;
+            int difference = int.MaxValue;
+            Random random = new Random();
+
+            while (difference >= Math.Abs(k))
+            {
+                int randomNumber = random.Next(2, 11); // Генерируем случайное число от 2 до 10
+                sum += randomNumber;
+                count++;
+                susm += randomNumber.ToString()+",";
+                // Рассчитываем разницу только если у нас есть хотя бы два числа
+                if (count > 1)
+                {
+                    difference = Math.Abs(randomNumber - lastNumber);
+                }
+
+                lastNumber = randomNumber;
+            }
+
+           
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows;
+using Lib_1;
+
+
+namespace RandomNumberDifferenceApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            // Закрываем приложение
+            Close ();
+        }
+
+        private void About_Click(object sender, RoutedEventArgs e)
+        {
+            // Инфа о программе
+            MessageBox.Show("Разработчик: [Баукин Алексей Алексеевич]\nНомер работы: [#1]\nЗадание:Вычислить разницу целых случайных чисел, распределенных в диапазоне от 2 до\r\n10, пока эта разница не станет меньше некоторого числа K (K<0). Вывести на экран\r\nсгенерированные числа, значение суммы, и количество сгенерированных чисел.", "О программе");
+        }
+
+        private void CalculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Проверка ввода на корректность
+            if (!int.TryParse(KInput.Text, out int k) || k >= 0)
+            {
+                MessageBox.Show("Пожалуйста, введите значение K, меньше 0.", "Ошибка");
+                return;
+            }
+
+            //  Для расчета из библиотеки
+          
+           Class1.CalculateDifference(k,out int d , out string ss , out int dd);
+
+            // Вывод результата 
+            ResultTextBlock.Text = $"Сгенерированные числа: {string.Join(",", ss)}\nСумма: {d}\nКоличество сгенерированных чисел: {dd}";
+        }
+    }
+}
